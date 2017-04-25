5756df9b328338d1e492f286ba9a0e3ad035bed3
jrviray <jrviray@cpp.edu>
2017-04-24 20:32:30 -0700

    initial file push

---
+++ app/src/main/java/edu/cpp/l05_http/BusService.java
@@ -0,0 +1,21 @@
+package edu.cpp.l05_http;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by yusun on 4/19/17.
+ */
+
+public interface BusService {
+
+    @GET("/Route/3164/Vehicles")
+    Call<List<RouteInfo>> getRouteAVehicles();
+
+    @GET("/Route/4512/Vehicles")
+    Call<List<RouteInfo>> getRouteBVehicles();
+
+
+}
